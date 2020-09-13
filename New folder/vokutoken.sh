87d0e6fbfebf880bce77952ec9ca49f5f37e6079
sayidina ahmadal qososyi <58449456+oendnsk675@users.noreply.github.com>
2020-09-13 22:10:15 +1400

    Delete vokutoken.sh

--- New folder/vokutoken.sh
+++
@@ -1,4 +0,0 @@
-api=$1
-cur=`curl -H "Accept: text/html,application/xhtml+xml,application/xml" \
-"http://api.scraperapi.com/?api_key=$api&url=https://www.spotify.com/id/password-reset/&keep_headers=true" -s`
-echo $cur
